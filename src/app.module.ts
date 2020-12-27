a68edd8bdba47c049afc5b36419b52110b06c5ba
konopek1 <michal.konopka01@student.wat.edu.pl>
2020-12-27 01:29:12 +0100

    Use nestjs/config instead of dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
+import Joi from '@hapi/joi';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AlgorandModule } from './algorand/algorand.module';
 
+const validationSchema = Joi.object({
+  POSTGRES_HOST: Joi.string().required(),
+  POSTGRES_PORT: Joi.number().required(),
+  POSTGRES_USER: Joi.string().required(),
+  POSTGRES_PASSWORD: Joi.string().required(),
+  POSTGRES_DB: Joi.string().required(),
+  PORT: Joi.number(),
+  ALGORAND_TOKEN: Joi.string().required(),
+  ALGORAND_URL: Joi.string().required(),
+  ALGORAND_PORT: Joi.number().required()
+});
+
 @Module({
-  imports: [AlgorandModule],
+  imports: [AlgorandModule, ConfigModule.forRoot({validationSchema})],
   controllers: [],
   providers: [],
 })
