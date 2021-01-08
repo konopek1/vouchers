a49fb883244623cdf0d485aa625ec6f97c8cda5d
konopek1 <michal.konopka01@student.wat.edu.pl>
2021-01-08 13:53:18 +0100

    Fix spaces in AppController :)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,9 +2,11 @@ import { Controller, Get, HttpCode } from '@nestjs/common';
 
 @Controller('')
 export default class AppController {
+  
   @Get('status')
   @HttpCode(200)
-  getStatus() {
+  public getStatus() {
+    
     return 'ok';
   }
 }
