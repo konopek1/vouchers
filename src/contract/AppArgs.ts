c00f5a5d5d7b78344693a3944c723a21f9532b76
konopek1 <michal.konopka01@student.wat.edu.pl>
2021-01-14 13:54:44 +0100

    Refactor AppArgs to accept also numbers

--- src/contract/AppArgs.ts
+++ src/contract/AppArgs.ts
@@ -1,9 +1,14 @@
+import { HttpException, HttpStatus } from "@nestjs/common";
+import { type } from "os";
+
+type ArgValue = number | string;
+
 
 export class AppArgs {
 
-    public args: string[];
+    public args: ArgValue[];
 
-    constructor(...args: string[]) {
+    constructor(...args: ArgValue[]) {
         this.args = args;
     }
 
@@ -12,6 +17,12 @@ export class AppArgs {
     }
 }
 
-export function AppArg(value: string) {
-    return new Uint8Array(Buffer.from(value));
+export function AppArg(value: ArgValue) {
+    if(typeof value === "string") {
+        return new Uint8Array(Buffer.from(value));
+    } else if (typeof value === "number") {
+        return new Uint8Array([value]);
+    } else {
+        throw new HttpException("App arg have to be string|number", HttpStatus.BAD_REQUEST);
+    }
 }
