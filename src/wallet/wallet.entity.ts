2e08ff84f33b63cf007de17125b855f4a30175f1
konopek1 <michal.konopka01@student.wat.edu.pl>
2021-01-27 23:38:49 +0100

    Add metadata property to WalletEntity

--- src/wallet/wallet.entity.ts
+++ src/wallet/wallet.entity.ts
@@ -1,4 +1,3 @@
-import { ConflictException } from "@nestjs/common";
 import { Asa } from "src/asa/asa.entity";
 import User from "src/user/user.entity";
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
@@ -8,7 +7,6 @@ export default class Wallet {
 
     @PrimaryGeneratedColumn()
     id: number;
-
 
     @Column()
     publicKey: string;
@@ -21,4 +19,15 @@ export default class Wallet {
 
     @ManyToOne(() => User, (user: User) => user.wallets, { eager: true})
     owner: User;
+
+    @Column({
+        type: 'jsonb',
+        nullable: true
+    })
+    metadata: Metadata;
+}
+
+export interface Metadata {
+    iterations: number;
+    nonce: string;
 }
