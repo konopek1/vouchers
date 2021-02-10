18a205ee84f30501fd2bc8ef1a111aa0b0f30e93
konopek1 <michal.konopka01@student.wat.edu.pl>
2021-02-10 14:22:25 +0100

    Add new app to cors

--- src/corsSettings.ts
+++ src/corsSettings.ts
@@ -1,6 +1,7 @@
 export default {
     origin: [
-      process.env.FRONT_APP_URL,
+      process.env.USER_APP_URL,
+      process.env.ADMIN_APP_URL
     ],
     credentials: true,
     exposedHeaders: ["set-cookie"],
